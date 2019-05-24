43ae5bf4187681143e938f3461f236183a604ec9
shanmati <45995908+Shanmati@users.noreply.github.com>
2019-05-24 10:34:05 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+sha=input()
+if (sha=='a' or sha =='e' or sha=='i' or sha=='o' or sha=='u' or sha=='A' or sha=='E' or sha=='I' or sha == 'O' or sha == 'U'):
+  print("Vowel")
+elif(sha=='!' or sha=='@' or sha=='#' or sha=='$' or sha=='%' or sha =='^' or sha=='&' or sha=='*')\
+  print("invalid")
+else:
+  print("Consonant")
