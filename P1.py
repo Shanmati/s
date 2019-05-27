814bb42c105b45300cf8911c6085e13d7424b702
shanmati <45995908+Shanmati@users.noreply.github.com>
2019-05-28 04:10:04 +0530

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,10 +1,13 @@
-nu=int(input())
-li=[]
-for i in range(n):
-  li.append(input())
-minilen=len(min(li,key=len))
-for i in range(len(li)-1):
-  for j in range(minilen):
-    if li[i][:j+1] in li[i+1]:
-      t=li[i][:j+1]
-print(t)
+su=input()
+su=int(su)
+l=[]
+for i in range(0,su):
+    s1=input()
+    l.append(s1)
+Z=[]
+for i in zip(*l):
+    if i.count(i[0])==len(i):
+        Z.append(i[0])
+    else:
+        break
+print(''.join(Z))
