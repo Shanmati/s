a272202a69b69d19133f92e7e0d4fb77c2ca310c
shanmati <45995908+Shanmati@users.noreply.github.com>
2019-05-31 11:45:03 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,11 +1,12 @@
-zaw=int(input())
-a=zaw
-s=0
-while zaw>0:
-    r=zaw%10
-    s=s+r**3
-    za=zaw//10
-if s==a:
+numi = int(input(""))
+sum = 0
+temp = numi
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+if numi == sum:
     print("yes")
 else:
     print("no")
