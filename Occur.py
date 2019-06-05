020aab5ade1ddd060ebd8f044fd2b3b17ac81179
shanmati <45995908+Shanmati@users.noreply.github.com>
2019-06-05 21:44:31 +0530

    Update Occur.py

--- Occur.py
+++ Occur.py
@@ -1,10 +1,10 @@
-m,n=raw_input().split()
+m,n=input().split()
 ob=abs(len(m)-len(n))
 for i in range(len(m)):
 	if len(n)==1 and n[i] in m:
 		break
-	if i>=len(m) or i>=len(n):
+	elif i>=len(m) or i>=len(n):
 		break
-	if n[i]!=n[i] and n[i]:
+	elif n[i]!=n[i] and n[i]:
 		ob=ob+1
 print(ob)
