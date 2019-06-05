16064d3257a63fa82c5cc4f660c218c861cfaac3
shanmati <45995908+Shanmati@users.noreply.github.com>
2019-06-05 22:21:02 +0530

    Update Eveninoddpos.py

--- Eveninoddpos.py
+++ Eveninoddpos.py
@@ -1,13 +1,13 @@
-nu=int(input())
-a = [int(y) for y in input().split()]
-for i in range(0,nu):
-    if i<nu-1:
-        b=' '
+nm=int(input())
+a = [int(x) for x in input().split()]
+for i in range(0,nm):
+    if i<nm-1:
+        ki=' '
     else:
-        b=''
+        ki=''
     if i%2==0:
         if a[i]%2!=0:
-            print(a[i],end=b)
+            print(a[i],end=ki)
     elif i%2!=0:
         if a[i]%2==0:
-            print(a[i],end=b)
+            print(a[i],end=ki)
